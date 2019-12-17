fc461dded6d68508203f5b3373323daf751a5df9
DavidJ2021 <jjeong2021@chadwickschool.org>
2019-12-17 12:32:18 +0900

    Implemented p_metric and r_metric

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from model import Model
 
 data = keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = data.load_data()
-model = Model(32, 64, 128, 256, 512, 10)
+model = Model(25, 10)
 model.fit(x_train, y_train)
-print(model.evaluate(x_test, y_test))
